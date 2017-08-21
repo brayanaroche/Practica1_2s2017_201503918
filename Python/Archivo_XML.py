38bc1a507a4f85fdaec7f6f721fc249bb41073ce
brayanaroche <30421259+brayanaroche@users.noreply.github.com>
2017-08-20 18:47:25 -0600

    Subir

--- Python/Archivo_XML.py
+++ Python/Archivo_XML.py
@@ -1,7 +1,27 @@
-from xml.dom import minidom
-xmldoc = minidom.parse('ArchivoXML.xml')
-itemlist = xmldoc.getElementsByTagName('nodos')
-print(len(itemlist))
-print(itemlist[0].attributes['name'].value)
-for s in itemlist:
-    print(s.attributes['name'].value)
+col = cola.listaCola()
+		colaMen = colaMensaje.listaColaMensaje()
+
+		archivo = open(cadena, "r") 
+		cadena= archivo.read()
+		xmldoc = minidom.parseString(cadena)
+		itemlist = xmldoc.getElementsByTagName("IP")
+		for i in itemlist:
+		    print ("ip= "+i.firstChild.nodeValue.strip())
+		    col.add(i.firstChild.nodeValue.strip())
+		itemlist = xmldoc.getElementsByTagName("texto")
+		for i in itemlist:
+		    print ("texto= "+i.firstChild.nodeValue.strip())
+		    colaMen.add(i.firstChild.nodeValue.strip())
+		colaMen.recorrer()
+		col.recorrer()
+
+cadena= archivo.read()
+		xmldoc = minidom.parseString(cadena)
+		itemlist = xmldoc.getElementsByTagName("IP")
+		for i in itemlist:
+		    print ("ip= "+i.firstChild.nodeValue.strip())
+		    col.add(i.firstChild.nodeValue.strip())
+		itemlist = xmldoc.getElementsByTagName("texto")
+		for i in itemlist:
+		    print ("texto= "+i.firstChild.nodeValue.strip())
+		    colaMen.add(i.firstChild.nodeValue.strip())
